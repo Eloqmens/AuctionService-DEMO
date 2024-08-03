7e9ab2817be7f6dc9df7fd8af64ac6ac3edc220d
Eloqmens <mesutovmuhammed@gmail.com>
2024-08-03 03:22:23 +0300

    Add DbContext

--- AuctionService.Domain/Entities/Auction.cs
+++ AuctionService.Domain/Entities/Auction.cs
@@ -8,6 +8,10 @@ namespace AuctionService.Domain.Entities
 {
     public class Auction
     {
-
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal StartingPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 }
---
+++ AuctionService.Infrastructure/Data/AuctionDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AuctionService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionService.Infrastructure.Data
+{
+    public class AuctionDbContext : DbContext
+    {
+        public AuctionDbContext(DbContextOptions<AuctionDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
